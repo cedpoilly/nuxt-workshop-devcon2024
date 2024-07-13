d0a76fdf52c632fe1e87d92179b0576ff8d5d87c
Sandeep Ramgolam <sandeep@ramgolam.com>
2024-07-13 20:46:58 +0400

    add tailwind

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # nuxt-workshop-devcon2024
+
+1. Setup Nuxt
+2. Clone from stackblitz
+3. Install TailwindCSS
--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="bg-green-500">
+    Congratulations ! You have successfully created your first Nuxt App
   </div>
 </template>
